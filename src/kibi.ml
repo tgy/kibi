616fb1d77c8328a919659a1bd9a8737fb8737e91
Grimpow <Grimpow@GPW.fr>
2013-12-07 18:12:06 +0100

    First step of main program

--- src/kibi.ml
+++ src/kibi.ml
@@ -1,1 +1,19 @@
+let updatestatus url msg =
+	let oo = open_out_gen [Open_creat;Open_append] 0o777 url in
+	Printf.fprintf oo "%s\n" msg;
+	close_out oo
 
+
+let main () =
+begin
+	let input_file = ref "input/default.png"
+	and output_dir = ref "output/"
+	in
+	Arg.parse [
+		("-i", Arg.String(fun s -> input_file := s), "input image");
+		("-o", Arg.String(fun s -> output_dir := s), "output dir")
+	] (fun _ -> ()) "kibi.native [-i input_file] [-o output_dir]";
+	
+end
+
+let _ = main ()
