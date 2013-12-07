d4ee069ce829c2b0814cd5aae700af243c772504
toogy <valentin.iovene@epita.fr>
2013-12-07 12:34:29 +0100

    export

--- src/guy/js/export.js
+++ src/guy/js/export.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
+
+  $("#export-button").click(function() {
+    var result = "";
+    $("#result > span > input").each(function() {
+      result += $(this).val() + " ";
+    });
+    alert(result);
+  });
+
 });
