6c5b8cb39f9cff8b68ae7d09a67892b68187101a
Grimpow <Grimpow@GPW.fr>
2013-12-07 16:27:30 +0100

    new Robert

--- src/test.ml
+++ src/test.ml
@@ -1,4 +1,12 @@
 let main () =
-  Setgen.generate "setgen/fonts" "setgen/out"
+	let output = "output.json" in
+	let line = ["bongour";"voir";"salope"]
+	and line2 = ["fleurissons";"vairre";"cheine"] in
+	let para = [line;line2] in
+	let whole = [para] in 
+  Robert.detect whole;
+	let l = Robert.correct whole in
+	Printf.printf "%s\n%!" (Robert.tojson l);
+	Robert.savejson output l
 
 let _ = main ()
