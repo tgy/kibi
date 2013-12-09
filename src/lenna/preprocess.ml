19082064273e49e29ce960f01a29e29461977f48
Rusty <mathieu.corre@wanadoo.fr>
2013-12-10 00:54:09 +0100

    freddy

--- src/lenna/preprocess.ml
+++ src/lenna/preprocess.ml
@@ -3,8 +3,8 @@ let preprocess img =
 	let preprocessed = HoughPreprocess.get_dotted binarized in
 	let theta = HoughTransform.get_angle preprocessed in
 	let rotated = Binarizator.binarize (Rotation.rotate binarized theta) in
-	let boxes = BoundingBoxes.get_boxes rotated () in
-	let mwidth, mheight = BoundingBoxes.average_box boxes in
+	(*let boxes = BoundingBoxes.get_boxes rotated () in*)
+	(*let mwidth, mheight = BoundingBoxes.average_box boxes in*)
 	(*let boxes = BoundingBoxes.remove_small_and_large boxes (mwidth, mheight) in
 	let removed = (BoundingBoxes.display_boxes rotated boxes) in*)
 	(binarized,preprocessed,rotated)
